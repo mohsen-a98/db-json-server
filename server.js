0df12186741667452a1a7f59f1571293fc446470
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-02-13 21:08:48 +0330

    add cors and json-serve package

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use("/api", router);
 server.listen(process.env.PORT || 5000, () => {
